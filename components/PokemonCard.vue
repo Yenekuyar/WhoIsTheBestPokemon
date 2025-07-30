ef18941bdecdbcdec3739e4fd7c71191858979e1
Daniel Coelho <62443963+Yenekuyar@users.noreply.github.com>
2025-07-29 23:48:40 -0300

    feat (WIP): Placeholders images on pokemon card

--- components/PokemonCard.vue
+++ components/PokemonCard.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="pokemon-card">
-    <h2>Pikachu</h2>
-    
-  </div>
+  <section class="pokemon-card">
+    <h2>Bulbasaur</h2>
+    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="" width="400">
+    <footer>
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/types/generation-ix/scarlet-violet/4.png" alt="">
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/types/generation-ix/scarlet-violet/12.png" alt="">
+    </footer>
+  </section>
 </template>
 
 <style scoped>
 .pokemon-card {
-  background-color: #f9f9f9;
-  border: 1px solid #eee;
   border-radius: 8px;
   padding: 12px;
   height: 100%;
   margin: 8px;
   text-align: center;
+  border: 8px solid #222;
+  image-rendering: pixelated;
+  box-shadow: 0 0 0 4px #fff, 0 0 0 8px #222;
+  border-radius: 20px;
 }
 </style>
