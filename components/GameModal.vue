a2960aad668efb37917eefa43925e5b9361adc5c
Daniel Coelho <62443963+Yenekuyar@users.noreply.github.com>
2025-08-03 23:23:23 -0300

    feat: add service on game modal

--- components/GameModal.vue
+++ components/GameModal.vue
@@ -1,13 +1,37 @@
 <template>
   <div class="game-modal-component">
     <section>
-      <PokemonCard />
+      <PokemonCard v-if="player1Pokemon" :randomPokemon="player1Pokemon" />
     </section>
     <section>
-      <PokemonCard />
+      <PokemonCard v-if="player2Pokemon" :randomPokemon="player2Pokemon" />
     </section>
   </div>
 </template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import { grabRandomPokemon } from '~/library';
+import { PokemonAPI } from '~/services';
+import type { IPokemon } from '~/types';
+import PokemonCard from './PokemonCard.vue';
+
+const player1Pokemon = ref<IPokemon>();
+const player2Pokemon = ref<IPokemon>();
+
+const pokemons = ref<IPokemon[]>([]);
+const remainingPokemons = ref<IPokemon[]>([]);
+
+onMounted(async () => {
+  const pokemonAPI = new PokemonAPI();
+  const pokemonsResponse = await pokemonAPI.getAllPokemonsData();
+  pokemons.value = pokemonsResponse;
+  remainingPokemons.value = pokemonsResponse;
+
+  player1Pokemon.value = grabRandomPokemon(remainingPokemons.value)[0];
+  player2Pokemon.value = grabRandomPokemon(remainingPokemons.value)[1];
+})
+</script>
 
 <style scoped>
 .game-modal-component {
