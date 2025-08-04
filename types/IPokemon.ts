495ff1a7fff7fe19b89ded2b68d8a270cfcc0eb5
Daniel Coelho <62443963+Yenekuyar@users.noreply.github.com>
2025-08-03 23:22:34 -0300

    feat: add property on IPokemon

--- types/IPokemon.ts
+++ types/IPokemon.ts
@@ -1,4 +1,10 @@
 export interface IPokemon {
   name: string;
   url: string;
+  sprites?: {
+    front_default: string;
+    back_default: string;
+    front_shiny?: string;
+    back_shiny?: string;
+  }
 }
