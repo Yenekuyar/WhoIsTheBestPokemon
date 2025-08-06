00851bdd284f3d64433e987d059bc7f8daa95d38
Daniel Coelho <62443963+Yenekuyar@users.noreply.github.com>
2025-08-05 22:54:53 -0300

    feat: add pokemon card types

--- components/PokemonCard.vue
+++ components/PokemonCard.vue
@@ -1,15 +1,17 @@
 <template>
   <section class="pokemon-card">
-    <div class="pokemon-card-image">
-      <img :src="randomPokemon?.url" alt="Pokemon Image" />
-    </div>
-    <div class="pokemon-card-name">
-      {{ randomPokemon?.name }}
-    </div>
+    <header class="card-header">{{ capitalize(randomPokemon?.name) }}</header>
+    <main class="pokemon-card-image">
+      <img :src="randomPokemon?.url" :alt="`Pokemon Image of ${randomPokemon?.name}`" style="width: 50%;" />
+    </main>
+    <footer class="card-types">
+      <img v-for="types in randomPokemon?.types" :src="`/img/pokemonTypes/${types?.type.name}.png`" :alt="`Move Image of ${types?.type.name}`" />
+    </footer>
   </section>
 </template>
 
 <script setup lang="ts">
+import capitalize from '~/utils/capitalize';
 import { defineProps } from 'vue'
 import { PokemonAPI } from '~/services';
 import type { IPokemon } from '~/types/IPokemon'
@@ -22,20 +24,50 @@ const pokemonApi = new PokemonAPI();
 const pokemonData = await pokemonApi.getSinglePokemonData(props.randomPokemon.name);
 
 props.randomPokemon.url = pokemonData.sprites?.front_default ?? '';
+props.randomPokemon.types = pokemonData.types ?? [];
 
 </script>
 
 <style scoped>
 .pokemon-card {
-  border-radius: 8px;
-  padding: 12px;
+  display: flex;
+  width: 100%;
+  flex-direction: column;
   height: 100%;
-  margin: 8px;
-  color: #fff;
-  text-align: center;
-  border: 8px solid #222;
-  image-rendering: pixelated;
-  box-shadow: 0 0 0 4px #fff, 0 0 0 8px #222;
-  border-radius: 20px;
+  translate: -6px -6px;
+  background: #343435;
+  border: 3px solid #000000;
+  box-shadow: 12px 12px 0 #000000;
+  overflow: hidden;
+  transition: all 0.3s ease;
+}
+
+.pokemon-card-image {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.card-header {
+  font-family: Montserrat, sans-serif;
+  font-size: 14px;
+  font-weight: 900;
+  width: 100%;
+  height: 32px;
+  background: #ffffff;
+  padding: 5px 12px;
+  color: #000000;
+  border-bottom: 3px solid #000000;
+}
+
+.card-types {
+  flex: 1 1;
+  display: flex;
+  gap: 20px;
+  margin-bottom: 20px;
+  align-items: center;
+  justify-content: center;
 }
 </style>
